3a22f96023b67fb92ecebfddaa7d075e14783f91
Ibnu Sina W <punyaibun@gmail.com>
2016-02-18 09:49:23 +0700

    docs: Add instruction how to replace the site

--- app/src/main/java/id/gits/jetpackreader/api/util/Constant.java
+++ app/src/main/java/id/gits/jetpackreader/api/util/Constant.java
@@ -4,6 +4,7 @@ package id.gits.jetpackreader.api.util;
  * Created by ibun on 16/01/16.
  */
 public class Constant {
+    //TODO change droidindonesia.com to your site
     public static final String BASE_URL = "https://public-api.wordpress.com/rest/v1.1/sites/droidindonesia.com/";
 }
 
